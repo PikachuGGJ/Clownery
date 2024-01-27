aaafb85dbacdc66ec7a7769fe64c4b809a37809c
Sergio <2001sergiobr@gmail.com>
2024-01-27 11:41:27 +0100

    Corrdinado con main

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,7 +11,7 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	gravity = 0
 
 #func _input(event):
@@ -30,7 +30,8 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	
 	if Input.is_action_just_pressed("quit"):
-		get_tree().quit(0)
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		Telon.change_scene_to_file("res://scenes/menu.tscn")
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

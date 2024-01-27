272016fff5b9d89eb9dd46b6e24a01a9338f2f1e
PinGunter <salvaromero08@gmail.com>
2024-01-27 11:29:01 +0100

    testing

---
+++ scripts/cubo_test.gd
@@ -0,0 +1,15 @@
+extends MeshInstance3D
+
+@export var speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var x_axis = Input.get_axis("A","D")
+	var z_axis = Input.get_axis("W", "S")
+	position.x += x_axis
+	position.z += z_axis

c4881cc9206791dfb4ac1a62084db82a1f44dfc5
Miguel Garbin <garbinbataneromiguel@gmail.com>
2024-01-27 12:02:15 +0100

    colisiones del personaje

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,15 +4,13 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@onready var pivote = $CameraOrigin
 @export var sens = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	gravity = 0
+#func _ready():
+	#gravity = 0
 
 #func _input(event):
 	#if event is InputEventMouseMotion:
@@ -28,9 +26,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit(0)
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

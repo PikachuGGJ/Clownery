6326dddb78b4557857bc3092c6cbe01cc342041e
PinGunter <salvaromero08@gmail.com>
2024-01-27 21:05:03 +0100

    AAAAA

--- scripts/Generador.gd
+++ scripts/Generador.gd
@@ -1,5 +1,12 @@
 extends Node3D
 
+@export var speed = 1.0
+@export var force = 20.0
+@export var verticalSpeed = 3.0
+@export var posicion : Vector3
+@export var gravedad : Vector3 = Vector3.DOWN * 9.8
+@export var rotationSpeed : float = 1
+
 var tarta = preload("res://scenes/tarta.tscn")
 var instancia
 
@@ -15,6 +22,11 @@ func generar_Tarta():
 	var z = rng.randi_range(-20, 20)
 
 	instancia.posicion = Vector3(x, y, z)
+	instancia.force = force
+	instancia.verticalSpeed = verticalSpeed
+	instancia.gravedad = gravedad
+	instancia.rotationSpeed = rotationSpeed
+	instancia.speed = speed
 	#instancia.body_entered.connect(quitar_tarta)
 	
 	add_child(instancia)
--- scripts/movParabolico.gd
+++ scripts/movParabolico.gd
@@ -1,20 +1,23 @@
 extends Area3D
 
 
-const SPEED = 1.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
 var velocity = Vector3.ZERO
 
 #forward movement
+@export var speed = 1.0
 @export var force = 20.0
 @export var verticalSpeed = 3.0
 @export var posicion : Vector3
 @export var gravedad : Vector3 = Vector3.DOWN * 9.8
+@export var rotationSpeed : float = 1
 
 #track time
 var t = 0
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	position = posicion
@@ -28,14 +31,10 @@ func _process(delta):
 	if t > 10:
 		queue_free()
 	t += delta
-	position = position + delta * velocity * SPEED;
+	position = position + delta * velocity * speed;
 	velocity += delta * gravedad
-
-
-#func _physics_process(delta):
-	#velocity += gravity * delta * SPEED
-	#look_at(transform.origin + velocity.normalized(), Vector3.UP)
-	#move_and_collide(velocity * delta * SPEED)
+	
+	rotation += rotationSpeed * delta * Vector3(rng.randf_range(0,2*PI), rng.randf_range(0,2*PI), rng.randf_range(0,2*PI))
 
 func die(killer):
 	print(name + ": me mató " + killer)

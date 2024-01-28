46cf2552e50d3246c375e5a3654b9eaf74c5237c
PinGunter <salvaromero08@gmail.com>
2024-01-28 12:27:56 +0100

    a medias

--- scripts/ScoreManager.gd
+++ scripts/ScoreManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var currentPlayer : String
+@onready var requests = $requests
 
 var scores : Dictionary = {}
 
@@ -13,11 +14,23 @@ func set_player_name(pname):
 func store_score(score):
 	scores[currentPlayer] = score
 
+func to_http_string() -> String:
+	var list = []
+	for player in scores:
+		list.push_back({"PlayerName": player, "Score": scores[player]})
+	return JSON.stringify(list)
+	
 func save_data():
 	var file = FileAccess.open("user://settings.json", FileAccess.WRITE)
 	file.store_string(JSON.stringify(scores))
 	print(JSON.stringify(scores))
 	file.close()
+	var headers = ["Content-Type: application/json", 
+	"apiKey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbXJtYWdmd3FseGNkbmpvdnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0Mzg3ODUsImV4cCI6MjAyMjAxNDc4NX0.tbO21rGiKfyc0uhVKkDvCIpGWjaWJh2d5LAy29sVhSs",
+	"Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbXJtYWdmd3FseGNkbmpvdnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0Mzg3ODUsImV4cCI6MjAyMjAxNDc4NX0.tbO21rGiKfyc0uhVKkDvCIpGWjaWJh2d5LAy29sVhSs",
+	"Prefer: return=minimal"]
+	var json = to_http_string()
+	requests.request("https://ksmrmagfwqlxcdnjovsf.supabase.co/rest/v1/Scoreboard", headers, HTTPClient.METHOD_POST, json)
 	
 func load_data():
 	var file = FileAccess.open("user://settings.json", FileAccess.READ)
@@ -28,7 +41,9 @@ func load_data():
 	else:
 		print("No se enconró el fichero de guardado")
 		scores = {}
-
+	var headers = ["apiKey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbXJtYWdmd3FseGNkbmpvdnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0Mzg3ODUsImV4cCI6MjAyMjAxNDc4NX0.tbO21rGiKfyc0uhVKkDvCIpGWjaWJh2d5LAy29sVhSs",
+	"Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzbXJtYWdmd3FseGNkbmpvdnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0Mzg3ODUsImV4cCI6MjAyMjAxNDc4NX0.tbO21rGiKfyc0uhVKkDvCIpGWjaWJh2d5LAy29sVhSs"]
+	requests.request("https://ksmrmagfwqlxcdnjovsf.supabase.co/rest/v1/Scoreboard", headers, HTTPClient.METHOD_GET)
 func get_scoreboard() -> Array:
 	var ordered = []
 	for player in scores:
@@ -38,3 +53,10 @@ func get_scoreboard() -> Array:
 
 func get_current_score():
 	return scores[currentPlayer]
+
+
+func _on_requests_request_completed(result, response_code, headers, body):
+	print(result)
+	print(response_code)
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
--- scripts/scores.gd
+++ scripts/scores.gd
@@ -14,3 +14,4 @@ func _ready():
 
 func _on_back_pressed():
 	Telon.change_scene_to_file("res://scenes/menu.tscn")
+
